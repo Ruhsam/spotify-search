21fa9d9183af71178e749953d65a4dce52f023e4
Ruhsam <ruhsam@gmail.com>
2016-03-23 08:56:40 -0700

    js and html file adjustments made

--- js/app.js
+++ js/app.js
@@ -3,7 +3,25 @@ $(document).on('ready', function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
+  var apiKey = '6rqhFgbbKwnb9MLmUQDhG6'
+  var apiURL = 'https://api.spotify.com/v1/search?type=track&q='+ tracks + apiKey
 
-  // your code here
-
+$.ajax({
+	method: 'GET',
+	URL: apiURL,
+	data: {
+		api_key:apiKey,
+		q:''
+},
+	success: onSuccess,
+	error: onError
 });
+  function onSuccess(json){
+	console.log(json);
+	console.log(json.data);
+}
+  function onError(){
+	console.log('error to the max')
+}
+$("form").on("submit");
+ console.log($this.val());
